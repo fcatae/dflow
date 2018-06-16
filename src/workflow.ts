243524b5c34d6abd7e11e1aa7cb31c02c994e6c7
Fabricio Catae <fcatae@microsoft.com>
2018-06-15 23:57:52 -0300

    improve the typings

--- src/workflow.ts
+++ src/workflow.ts
@@ -1,24 +1,25 @@
 // implementation
-var current: any = {};
+var current: CodeBlock;
 var worklist: {[name: string]: CodeBlock} = {};
 
-export function workflow(name: string, func: Function, timeout: number) : any {
+export function workflow(name: string, func: Function, timeout: number): void {
     var workflow = new CodeBlock(name, func, timeout);
+    current = workflow;
     workflow.init();
-
+    
     worklist[name] = workflow;
 }
 
 export function code(name: string, func: Function, timeout?: number) : any {
-    current.codeblocks = current.codeblocks || {}
+    if(current == null) {
+        throw 'code block not defined'
+    }
 
-    var isReady = name in current.codeblocks;
-
-    if(!isReady) {
-        current.codeblocks[name] = func;
+    if(!current.isReady) {
+        current.addBlock(name, func);
     } else {
-        var codeDefinition = current.codeblocks[name];
-        codeDefinition();
+        var codeBlock = current.getBlock(name);
+        codeBlock();
     }    
 }
 
@@ -31,18 +32,40 @@ class CodeBlock {
     private name: string;
     private codeDefinition: Function;
     private childBlocks?: {[name:string]: Function};
+    private _isReady: Boolean = false;
 
     constructor(name: string, func: Function, timeout: number) {
         this.name = name;        
         this.codeDefinition = func;
     }
 
-    get isReady() { return this.childBlocks != undefined };
+    get isReady() { return this._isReady };
 
     init(): void {
         this.codeDefinition();
+        this._isReady = true;
     }
+
     exec(): void {
         this.codeDefinition();
+    }    
+
+    addBlock(name: string, block: Function) {
+        this.childBlocks = this.childBlocks || {}
+        if(this.childBlocks == undefined) {
+            this.childBlocks = {}
+        }
+        if(name in this.childBlocks) {
+            throw ('code block already defined: ' + name)
+        }
+        this.childBlocks[name] = block;
+    }
+    
+    getBlock(name: string) {
+        if(this.childBlocks == undefined) {
+            throw 'code block not initialized';
+        }
+            
+        return this.childBlocks[name];
     }
 }
